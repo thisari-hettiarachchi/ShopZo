a7ed699df2bd8b6cf068c201763e1af4858c1c2a
Thisari Hettiarachchi <msthisari@gmail.com>
2025-12-20 13:02:24 +0530

    up

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+console.log("MONGO_URI:", process.env.MONGODB_URI);
 
 const feedbackRoutes = require("./routes/feedbackRoutes");
 
@@ -16,7 +17,7 @@ app.use("/api/feedbacks", feedbackRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Error:", err));
 
