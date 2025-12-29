a8a03cf6f1998f475fe32b818a3bd348c514f6fc
Thisari Hettiarachchi <msthisari@gmail.com>
2025-12-29 14:44:27 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="text-center text-5xl ">Hello Tailwind</div>
+
+    </div>
+  )
+}
+
+export default App
