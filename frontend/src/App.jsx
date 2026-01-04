eef3e2d38d5c83d77752e5ffb588aad6b015d310
Thisari Hettiarachchi <msthisari@gmail.com>
2026-01-05 00:29:41 +0530

    add auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import AppRoutes from './routes/AppRoutes'
-import Navbar from './components/shared/Navbar'
-import Footer from './components/shared/Footer'
+import { useLocation } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import Navbar from './components/shared/Navbar';
+import Footer from './components/shared/Footer';
 
 export default function App() {
+  const location = useLocation();
+  const noHeaderFooter = location.pathname === '/auth'; // Hide Navbar/Footer for Auth
+
   return (
     <>
-      <Navbar />
+      {!noHeaderFooter && <Navbar />}
       <AppRoutes />
-      <Footer />
+      {!noHeaderFooter && <Footer />}
     </>
-  )
+  );
 }
