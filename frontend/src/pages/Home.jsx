ea84923c578187ccff5c2331cd0a25fb76295faf
Thisari Hettiarachchi <msthisari@gmail.com>
2026-01-08 17:27:25 +0530

    add flash sale section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -218,7 +218,7 @@ export default function Hero() {
               Shop All
             </button>
           </div>
-          
+
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
             {Array.from({ length: 5 }).map((_, idx) => (
               <div
