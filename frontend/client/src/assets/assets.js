f1fc486e5baf2f87415753635928b1b6cad7cca3
Thisari Hettiarachchi <msthisari@gmail.com>
2026-08-13 20:58:53 +0530

    update assets

--- frontend/client/src/assets/assets.js
+++ frontend/client/src/assets/assets.js
@@ -4,12 +4,8 @@ import payment from "./payment.png";
 import download from "./download.png";
 import LightHero from "./hero-light.png";
 import DarkHero from "./hero-dark.png";
-import lap from "./lap.jpg";
-import lap1 from "./lap1.jpg";
 import Contact from "./contact.jpg";
 import about from "./about.jpg";
-import flashSale from "./flashsale.jpg";
-import newArrival from "./NewArrival.jpg";
 
 
 export default { 
